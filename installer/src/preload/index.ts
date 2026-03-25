dffe0883061d1c8de739cbfb49f147152b445360
axiang <jzx_forever@126.com>
2026-03-26 03:33:56 +0800

    feat(installer): add preload script

---
+++ installer/src/preload/index.ts
@@ -0,0 +1,27 @@
+import { contextBridge, ipcRenderer } from 'electron'
+
+// 暴露安全的 API 给渲染进程
+contextBridge.exposeInMainWorld('electronAPI', {
+  // 窗口控制
+  minimizeWindow: () => ipcRenderer.send('window-minimize'),
+  closeWindow: () => ipcRenderer.send('window-close'),
+
+  // 路径获取
+  getAppPath: () => ipcRenderer.invoke('get-app-path'),
+  getResourcePath: () => ipcRenderer.invoke('get-resource-path'),
+
+  // 安装相关
+  selectDirectory: () => ipcRenderer.invoke('select-directory'),
+  checkDependency: (dep: string) => ipcRenderer.invoke('check-dependency', dep),
+  installDependency: (dep: string) => ipcRenderer.invoke('install-dependency', dep),
+  startInstallation: (config: object) => ipcRenderer.invoke('start-installation', config),
+  copyFiles: (src: string, dest: string) => ipcRenderer.invoke('copy-files', src, dest),
+  generateConfig: (config: object) => ipcRenderer.invoke('generate-config', config),
+  testDatabaseConnection: (config: object) => ipcRenderer.invoke('test-database-connection', config),
+  createShortcut: (path: string) => ipcRenderer.invoke('create-shortcut', path),
+
+  // 进度回调
+  onInstallProgress: (callback: (progress: any) => void) => {
+    ipcRenderer.on('install-progress', (_event, progress) => callback(progress))
+  }
+})
