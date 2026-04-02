809aaf1d217b186921dfe1eb9433538f48fb49b4
axiang <jzx_forever@126.com>
2026-04-02 12:53:52 +0800

    chore(db): 更新 drop_subagent_content 迁移文件日期为 2026-04-02

--- isdp/sql-change/migrations/202604020004_drop_subagent_content.sql
+++ isdp/sql-change/migrations/202604020004_drop_subagent_content.sql
@@ -1,7 +1,7 @@
--- isdp/sql-change/migrations/202603310003_drop_subagent_content.sql
+-- isdp/sql-change/migrations/202604020004_drop_subagent_content.sql
 -- 变更说明：移除 subagents 表的 content 字段（内容已存储在文件系统）
 -- 作者：ISDP Team
--- 日期：2026-03-31
+-- 日期：2026-04-02
 
 SET NAMES utf8mb4;
 
