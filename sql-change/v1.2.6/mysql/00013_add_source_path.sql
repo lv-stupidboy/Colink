9a3c43f8218c3de5a6f022ed354afe479a0fd43d
yangyang <704267083@qq.com>
2026-05-08 17:28:20 +0800

    sql脚本目录移动

--- sql-change/v1.2.6/mysql/00013_add_source_path.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE skills ADD COLUMN source_path VARCHAR(255) DEFAULT '';
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE skills DROP COLUMN source_path;
--- +goose StatementEnd
