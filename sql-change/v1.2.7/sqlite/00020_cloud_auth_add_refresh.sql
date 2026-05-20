6b7acaff0cda3d72d81171a25900e2b3ef272443
justin <justin@justindeMac-mini-2.local>
2026-05-20 15:00:40 +0800

    fix: remove cloud-related SQL files from v1.2.7, keep only memory files

--- sql-change/v1.2.7/sqlite/00020_cloud_auth_add_refresh.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
-ALTER TABLE cloud_auth ADD COLUMN refresh_token TEXT NOT NULL DEFAULT '';
-ALTER TABLE cloud_auth ADD COLUMN token_expires_at DATETIME;
-
--- +goose Down
-ALTER TABLE cloud_auth DROP COLUMN refresh_token;
-ALTER TABLE cloud_auth DROP COLUMN token_expires_at;
